25f80311b090f9d73d987cb50889441c5b8d303c
bine <bine.iljas1@gmail.com>
2018-01-12 13:17:06 +0100

    order dodal narocnika in izdelke

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -2,11 +2,22 @@
 
 @section('content')
     <div class="container">
+        NAROČNIK:
         {{$user}}
-        @foreach($shopping_bag as $article)
-            {{$article}}
-        @endforeach
+        <ul>
+            <li>{{$user->firstname}}</li>
+            <li>{{$user->lastname}}</li>
+            <li>{{$user->email}}</li>
+            <li>{{$user->address}}</li>
 
+        </ul>
+
+        <ul>
+        @foreach($shopping_bag as $article)
+            <li>{{$article->name}}  količina:{{$article->quantity}}</li>
+
+        @endforeach
+        </ul>
         <button class="btn btn-success">Finish</button> {{--TODO: POST to route(order.store)--}}
     </div>
 @endsection
