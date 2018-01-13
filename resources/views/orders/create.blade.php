82b40bed564fcd998f1eaa365aced24274cd983f
bine <bine.iljas1@gmail.com>
2018-01-13 11:08:09 +0100

    dodal

--- resources/views/orders/create.blade.php
+++ resources/views/orders/create.blade.php
@@ -2,22 +2,40 @@
 
 @section('content')
     <div class="container">
-        NAROČNIK:
-        {{$user}}
-        <ul>
-            <li>{{$user->firstname}}</li>
-            <li>{{$user->lastname}}</li>
-            <li>{{$user->email}}</li>
-            <li>{{$user->address}}</li>
+        NAROČNIK: <b>{{$user->firstname}}</b>
+
+        <ul class="list-group">
+            <li class="list-group-item">{{$user->firstname}}</li>
+            <li class="list-group-item">{{$user->lastname}}</li>
+            <li class="list-group-item">{{$user->email}}</li>
+            <li class="list-group-item">{{$user->address}}</li>
 
         </ul>
+        <br>
+        <?php
 
-        <ul>
+        $var = 0;
+        ?>
+
+        <ul class="list-group">
         @foreach($shopping_bag as $article)
-            <li>{{$article->name}}  količina:{{$article->quantity}}</li>
+            <li class="list-group-item">
+                <b>{{$article->name}}</b>
+                <ul>
+                    <li>količina: {{$article->quantity}}</li>
+                    <li>cena enega izdelk: {{$article->price}}€ </li>
+                    <li>cena vseh naročenih : {{$article->price*$article->quantity}}€</li>
+                    <?php
 
+                    $var = $var + $article->quantity*$article->price;
+                    ?>
+                </ul>
+            </li>
         @endforeach
+            <li class="list-group-item"> CENA: {{$var}} €</li>
         </ul>
+
+
         <button class="btn btn-success">Finish</button> {{--TODO: POST to route(order.store)--}}
     </div>
 @endsection
