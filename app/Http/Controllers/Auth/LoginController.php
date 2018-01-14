af10d93c4b854691f03e6dcd157e059a193d6356
Domen Jeric <jeric.domen@gmail.com>
2018-01-14 13:45:09 +0100

    x509 certs working

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -46,6 +47,27 @@ class LoginController extends Controller
             Auth::logout();
             return redirect()->back()->withErrors(['Account inactive']);
         }
+
+        if (Auth::user()->role === User::ROLE_CUSTOMER) {
+            return redirect('/home');
+        }
+
+        if (!array_key_exists('SSL_CLIENT_S_DN_Email', $_SERVER)) {
+            Auth::logout();
+            return redirect()->back()->withErrors(['Please select your certificate.']);
+        }
+
+        $email = $_SERVER['SSL_CLIENT_S_DN_Email'];
+
+        if (!$email && preg_match('#/emailAddress=(.+\@.+\..+)(/|$)#', $_SERVER['SSL_CLIENT_S_DN'], $matches)) {
+            $email = $matches[1];
+        }
+
+        if (Auth::user()->email != $email) {
+            Auth::logout();
+            return redirect()->back()->withErrors(['Please select your certificate.']);
+        }
+
 
         return redirect('/home');
     }
